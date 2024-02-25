7b3281c713343556743d2dec112e5665207bd124
Erichard <erichard660@gmail.com>
2024-02-24 18:29:40 -0800

    app

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3001,NODE_ENV } = process.env;
 const app = express();
+const cors = require("cors");
+const { errors } = require("celebrate");
+const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { limiter } = require("./utils/limiter")
 
 mongoose.connect(
   "mongodb://127.0.0.1:27017/newsapp_db",
@@ -15,6 +22,23 @@ mongoose.connect(
 app.listen(PORT, () => {
     console.log(`App listening at port ${PORT}`);
   });
+
+  app.get('/crash-test', () => {
+    setTimeout(() => {
+      throw new Error('Server will crash now');
+    }, 0);
+  });
+
+  const routes = require("./routes");
+  app.use(express.json());
+  app.use(cors());
+  app.use(helmet());
+  app.use(limiter);
+  app.use(requestLogger);
+  app.use(routes);
+  app.use(errorLogger);
+  app.use(errors());
+  app.use(errorHandler);
   
 
   // "C:\Program Files\MongoDB\Server\6.0\bin\mongod.exe" --dbpath="c:\data\db
